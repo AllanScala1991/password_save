1dd9bea4c88fb9078424ab4104e946130a040284
Allan Scala <allan.scala@wisereducacao.com>
2021-03-30 18:41:49 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    minWidth: 1280,
+    minHeight: 800,
+    maxWidth: 1920,
+    maxHeight: 1080,
+    show: false,
+    frame: true,
+    fullscreen: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.maximize()
+  mainWindow.setMenuBarVisibility(false)
+  mainWindow.loadFile('app/html/app.html')
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+//electron-packager . --platform=win32  --archi=ia32 --icon="C:\Users\Allan Scala\Documents\Application\app\img\banknotes-32.ico"
+//electron-packager . --platform=darwin  --arch=x64 --icon="/Users/admin/Documents/Projetos/my-money-application/app/img/moneybox-32.icns"
